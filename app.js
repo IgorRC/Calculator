1e005c92a0504c63017275ee5a809408b229976b
igor <igor.ramos.c@hotmail.com>
2022-07-11 15:26:11 -0500

    codigo agregados

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log(captura)
+const botonesNumeros = document.querySelectorAll('.num');
+const botonesOperaciones = document.querySelectorAll('.operation');
+let pantalla = document.getElementById("input");
+let disply = "";
+let anterior, actual;
+let operation;
+
+botonesNumeros.forEach(boton => {
+  boton.addEventListener('click', () => {
+      console.log(boton.innerHTML);
+      
+      pantalla.innerHTML = `${disply}`;
+      if(operation != null){
+        anterior += boton.innerHTML;
+      }
+  });
+})
+
+function isHaveOperation(op) {
+    if(op != null){
+        re
+    }
+}
+
+
+
+botonesOperaciones.forEach(boton => {
+  boton.addEventListener('click', () => {
+      operation = boton;
+  });
+})
