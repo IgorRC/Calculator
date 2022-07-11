79ed75942aa6c8f6d485989b95ba0aca1ce93d86
igor <igor.ramos.c@hotmail.com>
2022-07-11 15:38:16 -0500

    codigos agregads

--- app.js
+++ app.js
@@ -2,30 +2,20 @@ const botonesNumeros = document.querySelectorAll('.num');
 const botonesOperaciones = document.querySelectorAll('.operation');
 let pantalla = document.getElementById("input");
 let disply = "";
-let anterior, actual;
-let operation;
 
-botonesNumeros.forEach(boton => {
-  boton.addEventListener('click', () => {
-      console.log(boton.innerHTML);
-      
-      pantalla.innerHTML = `${disply}`;
-      if(operation != null){
-        anterior += boton.innerHTML;
-      }
+
+botonesNumeros.forEach(numero => {
+  numero.addEventListener('click', () => {
+    console.log(numero.innerHTML);  
+    disply += numero.innerHTML;
+    pantalla.innerHTML = `${disply}`;  
   });
 })
 
-function isHaveOperation(op) {
-    if(op != null){
-        re
-    }
-}
 
 
-
-botonesOperaciones.forEach(boton => {
-  boton.addEventListener('click', () => {
-      operation = boton;
+botonesOperaciones.forEach(operation => {
+  operation.addEventListener('click', () => {
+      
   });
 })
