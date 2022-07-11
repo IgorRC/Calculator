de9c3cde58bd25e923bae2c96aed6c44b4da81d9
Jimmy Loloy <loloy.laurencio@gmail.com>
2022-07-11 11:17:34 -0500

    capturar los datos de la calculadora

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// hola mundo cruel :u
+const botonesNumeros = document.querySelectorAll('.num');
+const botonesOperaciones = document.querySelectorAll('.operation');
+
+botonesNumeros.forEach(boton => {
+  boton.addEventListener('click', () => {
+      console.log(boton.innerHTML);
+  });
+})
+
+botonesOperaciones.forEach(boton => {
+  boton.addEventListener('click', () => {
+      console.log(boton.innerHTML);
+  });
+})
